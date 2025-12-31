1ba736b42a8d54de15177d568d57787175887aff
mostafakhaled88 <mosta.mk@gmail.com>
2025-12-31 04:50:40 +0200

    Update test_bronze.py

--- tests/test_bronze.py
+++ tests/test_bronze.py
@@ -1,15 +1,17 @@
 from dags.medallion.bronze_extract import extract_crypto_data
+import pytest
+from unittest.mock import patch, Mock
 
-def test_extract_crypto_data(mocker):
-    mock_response = [
-        {"symbol": "BTC", "price": 45000}
+@patch("dags.medallion.bronze_extract.requests.get")
+def test_extract_crypto_data(mock_get):
+    # Mock API response
+    mock_get.return_value = Mock()
+    mock_get.return_value.json.return_value = [
+        {"symbol": "BTC", "price": "45000"}
     ]
-
-    mocker.patch(
-        "dags.medallion.bronze_extract.requests.get",
-        return_value=mocker.Mock(json=lambda: mock_response)
-    )
 
     data = extract_crypto_data()
 
+    assert isinstance(data, list)
     assert data[0]["symbol"] == "BTC"
+    assert float(data[0]["price"]) == 45000
